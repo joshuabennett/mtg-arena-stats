abfbb45ab72d3d0ccfc9540d191fde562157b2fe
Joshua Bennett <joshuabennett@utexas.edu>
2020-06-20 12:33:48 -0500

    Added a small feature to use URL Parameters to autopopulate login fields.

--- src/components/signin/signin.jsx
+++ src/components/signin/signin.jsx
@@ -15,6 +15,28 @@ class SignIn extends React.Component {
     };
   }
 
+  componentDidMount() {
+    var urlVars = this.getUrlVars();
+    urlVars.forEach((arg) => {
+      this.setState(urlVars);
+    });
+  }
+  getUrlVars() {
+    var vars = [],
+      hash;
+    var hashes = window.location.href
+      .slice(window.location.href.indexOf("?") + 1)
+      .split("&");
+
+    for (var i = 0; i < hashes.length; i++) {
+      hash = hashes[i].split("=");
+      vars.push(hash[0]);
+      vars[hash[0]] = hash[1];
+    }
+
+    return vars;
+  }
+
   handleSubmit = async (e) => {
     e.preventDefault();
 
